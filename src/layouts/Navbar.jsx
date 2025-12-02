923a34d84c1a6d6a159e71e533c7021885b187b9
mdarifhossengh <myoffical430@gmail.com>
2025-12-02 07:43:06 -0600

    navbar

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -27,20 +27,22 @@ const Navbar = () => {
             <li><Link to="/" className='px-screen inline-block'>Home</Link></li>
             <li><Link to="/shop" className='px-full inline-block'>Shop</Link></li>
             <li><Link to="/contact" className='px-full inline-block'>contact</Link></li>
+            <li><Link to="/About" className='px-2 lg:px-5 inline-block'>About</Link></li>
+            <li><Link to="/Blog" className='px-2 lg:px-5 inline-block'>Blog</Link></li>
           </ul>
         </div>     
         <div className="flex gap-6 text-secondary items-center text-xl">
           <div className="flex flex-col sm:flex-row gap-4 sm:gap-6 justify-center items-center">
             <Link 
               to="/signUp" 
-              className="border border-primary px-5 py-2 text-white text-base sm:text-lg font-medium bg-[#0D1164] hover:scale-105 transition-transform rounded-2xl w-full sm:w-auto text-center"
+              className="border border-primary px-2 py-1 md:px-5 md:py-2 text-white text-base md:text-lg font-medium bg-[#0D1164] hover:scale-105 transition-transform rounded-2xl w-full sm:w-auto text-center"
             >
               Sign up
             </Link>
 
             <Link 
               to="/Login" 
-              className="border border-primary px-5 py-2 text-white text-base sm:text-lg font-medium bg-[#0D1164] hover:scale-105 transition-transform rounded-2xl w-full sm:w-auto text-center"
+              className="border border-primary px-2 py-1 md:px-5 md:py-2 text-white text-base md:text-lg font-medium bg-[#0D1164] hover:scale-105 transition-transform rounded-2xl w-full sm:w-auto text-center"
             >
               Login
             </Link>
