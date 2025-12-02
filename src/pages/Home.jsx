b195c89478931601623d5961dd23f11b468ed359
mdarifhossengh <myoffical430@gmail.com>
2025-12-02 07:20:58 -0600

    src/pages/Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Banner from '../component/home/Banner.jsx'
-import Popular from "../component/home/Popular.jsx";
+import Banner from "../components/home/Banner.jsx"
+import Popular from "../component/home/Popular.jsx"
 
 const Home = () => {
   return (
